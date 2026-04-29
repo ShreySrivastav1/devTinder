a7c38ecc1108c979417287aac34018a63a2b7b4c
Shrey Srivastav <shreysrivastav134678@gmail.com>
2026-04-29 13:27:54 +0530

    add ses on signup

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,6 @@ const dns = require("node:dns/promises");
 dns.setServers(["1.1.1.1"]);
 const mongoose = require("mongoose");
 
-console.log(process.env.DB_CONNECTION_SECRET)
-
 const connectDb = async() => { 
     await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
